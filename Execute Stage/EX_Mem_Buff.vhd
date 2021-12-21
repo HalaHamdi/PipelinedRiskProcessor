4cd6091392c9da0a8a70015d3ee3fe498134fda7
Halahamdy22 <halahamdyabdelhady@gmail.com>
2021-12-21 18:03:06 +0200

    Execute Buffer edited

--- Execute Stage/EX_Mem_Buff.vhd
+++ Execute Stage/EX_Mem_Buff.vhd
@@ -7,73 +7,48 @@ ENTITY ExMemBuff IS
 	     src1add_in,scr2add_in,destadd_in:IN std_logic_vector(2 DOWNTO 0);
 	     aluout_in,scr1_in,immediate_in:IN std_logic_vector (15 DOWNTO 0);
 	     pc_in:IN std_logic_vector (31 DOWNTO 0);
-		 
-		 writeback_in, ldm_in, port_read_in, mem_to_reg_in: IN std_logic;
-		 
-		 writeback_out, ldm_out, port_read_out, mem_to_reg_out: OUT std_logic;
-		 
-		 src1add_out,scr2add_out,destadd_out:OUT std_logic_vector(2 DOWNTO 0);
+	     writeback_in, ldm_in, port_read_in, mem_to_reg_in: IN std_logic;
+	     writeback_out, ldm_out, port_read_out, mem_to_reg_out: OUT std_logic;
+	     src1add_out,scr2add_out,destadd_out:OUT std_logic_vector(2 DOWNTO 0);
 	     aluout_out,scr1_out,immediate_out:OUT std_logic_vector (15 DOWNTO 0);
-	     pc_out:OUT std_logic_vector (31 DOWNTO 0);
-		 
+	     pc_out:OUT std_logic_vector (31 DOWNTO 0)
 		 );
 END ENTITY ExMemBuff ;
 
 ARCHITECTURE ExMemBuff OF ExMemBuff IS 
-signal  src1addS,scr2addS,destaddS :std_logic_vector(2 DOWNTO 0);
-signal  aluoutS,scr1S,immediateS :std_logic_vector (15 DOWNTO 0);
-signal  pcS :std_logic_vector (31 DOWNTO 0);
+
 
 --read anytime and write only at rising edge
 BEGIN
 	
 	PROCESS(clk)
-	Variable  src1addV,scr2addV,destaddV :std_logic_vector(2 DOWNTO 0);
-	Variable  aluoutV,scr1V,immediateV :std_logic_vector (15 DOWNTO 0);
-	Variable  pcV :std_logic_vector (31 DOWNTO 0);
 	BEGIN
 		IF (rst='1') THEN
-			src1add_in <=(OTHERS=>'0');
-			scr2addV:=(OTHERS=>'0');
-			destaddV:=(OTHERS=>'0');
-			aluoutV:=(OTHERS=>'0');
-			scr1V:=(OTHERS=>'0');
-			immediateV:=(OTHERS=>'0');
-			pcV:=(OTHERS=>'0');
-			writeback_out <= (others => '0'); 
-			ldm_out <= (others => '0');
-			port_read_out <= (others=>'0');
-			mem_to_reg_out <= (others=>'0');
+			writeback_out <='0';
+			ldm_out<='0';
+			port_read_out<='0';
+			mem_to_reg_out<='0';
+			src1add_out<=(OTHERS=>'0');
+			scr2add_out<=(OTHERS=>'0');
+			destadd_out<=(OTHERS=>'0');
+			aluout_out <= (others => '0'); 
+			scr1_out <= (others => '0');
+			immediate_out <= (others=>'0');
+			pc_out <= (others=>'0');
 		ELSIF(rising_edge(clk)) THEN
-			src1addV:=src1add;
-			scr2addV:=scr2add;
-			destaddV:=destadd;
-			aluoutV:=aluout;
-			scr1V:=scr1;
-			immediateV:=immediate;
-			pcV:=pc;
 			writeback_out <= writeback_in; 
 			ldm_out <= ldm_in;
 			port_read_out <= port_read_in;
 			mem_to_reg_out <= mem_to_reg_in;
+			src1add_out<=src1add_in;
+			scr2add_out<=scr2add_in;
+			destadd_out<=destadd_in;
+			aluout_out<=aluout_in;
+			scr1_out<=scr1_in;
+			immediate_out<=immediate_in;
+			pc_out<=pc_in;
 		END IF;
-		
-			src1addS<=src1addV;
-			scr2addS<=scr2addV;
-			destaddS<=destaddV;
-			aluoutS<=aluoutV;
-			scr1S<=scr1V;
-			immediateS<=immediateV;
-			pcS<=pcV;
 			
 	END PROCESS;
-
-src1add<=src1addS;
-scr2add<=scr2addS;
-destadd<=destaddS;
-aluout<=aluoutS;
-scr1<=scr1S;
-immediate<=immediateS;
-pc<=pcS;
 
 END ExMemBuff;
--- Execute Stage/ExStage.vhd
+++ Execute Stage/ExStage.vhd
@@ -6,11 +6,15 @@ ENTITY ExStage IS
 	PORT(alusrc,clk,rst:IN std_logic;
 	     aluop:IN std_logic_vector(2 DOWNTO 0);
 	     immediate,src1,src2:IN std_logic_vector(15 DOWNTO 0);
-		 writeback_in, ldm_in, port_read_in, mem_to_reg_in: IN std_logic;
-		 addr_Rsrc1_in, addr_Rsrc2_in, addr_Rdst_in: IN std_logic_vector(2 downto 0);
-		 pc_in: IN std_logic_vector(31 downto 0);
-		 
-		 alu_result: OUT std_logic_vector(15 downto 0)
+	     writeback_in, ldm_in, port_read_in, mem_to_reg_in: IN std_logic;
+	     addr_Rsrc1_in, addr_Rsrc2_in, addr_Rdst_in: IN std_logic_vector(2 downto 0);
+	     pc_in: IN std_logic_vector(31 downto 0);
+
+	     alu_result: OUT std_logic_vector(15 downto 0);
+	     writeback_out, ldm_out, port_read_out, mem_to_reg_out: OUT std_logic;
+	     src1add_out,scr2add_out,destadd_out:OUT std_logic_vector(2 DOWNTO 0);
+	     aluout_out,scr1_out,immediate_out:OUT std_logic_vector (15 DOWNTO 0);
+	     pc_out:OUT std_logic_vector (31 DOWNTO 0)
 		 );
 
 END ENTITY ExStage;
@@ -22,13 +26,15 @@ signal mux1result:std_logic_vector(15 DOWNTO 0);
 signal aluout:std_logic_vector(15 DOWNTO 0);
 signal aluflagsout:std_logic_vector(3 DOWNTO 0);
 signal flags:std_logic_vector(3 DOWNTO 0);
+
 BEGIN
 		
 mux1: entity work.exmux1 port map (alusrc,immediate,src2,mux1result);
-alu:  entity work.alu port map(aluop,src1,mux1result,flags,aluout,aluflagsout);
-flagreg:  entity work.flagreg port map('1',clk,rst,aluflagsout,flags);
+alu: entity work.alu port map(aluop,src1,mux1result,flags,aluout,aluflagsout);
+flagreg: entity work.flagreg port map('1',clk,rst,aluflagsout,flags);
 buff: entity work.ExMemBuff port map(clk, rst, 
-								addr_Rsrc1_in, addr_Rsrc2_in, addr_Rdst_in,
+				     addr_Rsrc1_in, addr_Rsrc2_in, addr_Rdst_in,
+				     aluflagsout
 								aluout)
 		
 END ExStage; 
