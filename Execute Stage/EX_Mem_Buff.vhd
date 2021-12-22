36b9af12bd4df2a1c12da4e28df367e3a0480434
NouranHany <hany.noran@yahoo.com>
2021-12-22 11:51:34 +0200

    Execute Buffer Added

--- Execute Stage/EX_Mem_Buff.vhd
+++ Execute Stage/EX_Mem_Buff.vhd
@@ -4,11 +4,14 @@ USE IEEE.std_logic_1164.all;
 
 ENTITY ExMemBuff IS
 	PORT(clk,rst:IN std_logic;
+	     stack_in:In std_logic_vector(1 DOWNTO 0);
 	     src1add_in,scr2add_in,destadd_in:IN std_logic_vector(2 DOWNTO 0);
 	     aluout_in,scr1_in,immediate_in:IN std_logic_vector (15 DOWNTO 0);
 	     pc_in:IN std_logic_vector (31 DOWNTO 0);
-	     writeback_in, ldm_in, port_read_in, mem_to_reg_in: IN std_logic;
-	     writeback_out, ldm_out, port_read_out, mem_to_reg_out: OUT std_logic;
+	     writeback_in, ldm_in, port_read_in, mem_to_reg_in,pc_to_stack_in,mem_write_in,mem_read_in,rti_in,int_in,ret_in,call_in: IN std_logic;
+
+	     writeback_out, ldm_out, port_read_out, mem_to_reg_out,pc_to_stack_out,mem_write_out,mem_read_out,rti_out,int_out,ret_out,call_out: OUT std_logic;
+	     stack_out:OUT std_logic_vector(1 DOWNTO 0);
 	     src1add_out,scr2add_out,destadd_out:OUT std_logic_vector(2 DOWNTO 0);
 	     aluout_out,scr1_out,immediate_out:OUT std_logic_vector (15 DOWNTO 0);
 	     pc_out:OUT std_logic_vector (31 DOWNTO 0)
@@ -24,10 +27,18 @@ BEGIN
 	PROCESS(clk)
 	BEGIN
 		IF (rst='1') THEN
+			stack_out<="00";
 			writeback_out <='0';
 			ldm_out<='0';
 			port_read_out<='0';
 			mem_to_reg_out<='0';
+			pc_to_stack_out<='0';
+			mem_write_out<='0';
+			mem_read_out<='0';
+			rti_out<='0';
+			int_out<='0';
+			ret_out<='0';
+			call_out<='0';
 			src1add_out<=(OTHERS=>'0');
 			scr2add_out<=(OTHERS=>'0');
 			destadd_out<=(OTHERS=>'0');
@@ -36,10 +47,18 @@ BEGIN
 			immediate_out <= (others=>'0');
 			pc_out <= (others=>'0');
 		ELSIF(rising_edge(clk)) THEN
+			stack_out<=stack_in;
 			writeback_out <= writeback_in; 
 			ldm_out <= ldm_in;
 			port_read_out <= port_read_in;
 			mem_to_reg_out <= mem_to_reg_in;
+			pc_to_stack_out<=pc_to_stack_in;
+			mem_write_out<=mem_write_in;
+			mem_read_out<=mem_read_in;
+			rti_out<=rti_in;
+			int_out<=int_in;
+			ret_out<=ret_in;
+			call_out<=call_in;
 			src1add_out<=src1add_in;
 			scr2add_out<=scr2add_in;
 			destadd_out<=destadd_in;
--- Execute Stage/ExStage.vhd
+++ Execute Stage/ExStage.vhd
@@ -5,13 +5,14 @@ USE IEEE.numeric_std.all;
 ENTITY ExStage IS 
 	PORT(alusrc,clk,rst:IN std_logic;
 	     aluop:IN std_logic_vector(2 DOWNTO 0);
-	     immediate,src1,src2:IN std_logic_vector(15 DOWNTO 0);
-	     writeback_in, ldm_in, port_read_in, mem_to_reg_in: IN std_logic;
+	     stack_in:In std_logic_vector(1 DOWNTO 0);
+	     immediate_in,src1_in,src2:IN std_logic_vector(15 DOWNTO 0);
+	     writeback_in, ldm_in, port_read_in, mem_to_reg_in,pc_to_stack_in,mem_write_in,mem_read_in,rti_in,int_in,ret_in,call_in: IN std_logic;
 	     addr_Rsrc1_in, addr_Rsrc2_in, addr_Rdst_in: IN std_logic_vector(2 downto 0);
 	     pc_in: IN std_logic_vector(31 downto 0);
 
-	     alu_result: OUT std_logic_vector(15 downto 0);
-	     writeback_out, ldm_out, port_read_out, mem_to_reg_out: OUT std_logic;
+	     writeback_out, ldm_out, port_read_out, mem_to_reg_out,pc_to_stack_out,mem_write_out,mem_read_out,rti_out,int_out,ret_out,call_out: OUT std_logic;
+	     stack_out:OUT std_logic_vector(1 DOWNTO 0);
 	     src1add_out,scr2add_out,destadd_out:OUT std_logic_vector(2 DOWNTO 0);
 	     aluout_out,scr1_out,immediate_out:OUT std_logic_vector (15 DOWNTO 0);
 	     pc_out:OUT std_logic_vector (31 DOWNTO 0)
@@ -26,17 +27,24 @@ signal mux1result:std_logic_vector(15 DOWNTO 0);
 signal aluout:std_logic_vector(15 DOWNTO 0);
 signal aluflagsout:std_logic_vector(3 DOWNTO 0);
 signal flags:std_logic_vector(3 DOWNTO 0);
+ 
 
-BEGIN
-		
-mux1: entity work.exmux1 port map (alusrc,immediate,src2,mux1result);
-alu: entity work.alu port map(aluop,src1,mux1result,flags,aluout,aluflagsout);
+BEGIN		
+mux1: entity work.exmux1 port map (alusrc,immediate_in,src2,mux1result);
+alu: entity work.alu port map(aluop,src1_in,mux1result,flags,aluout,aluflagsout);
 flagreg: entity work.flagreg port map('1',clk,rst,aluflagsout,flags);
 buff: entity work.ExMemBuff port map(clk, rst, 
+					 stack_in,
 				     addr_Rsrc1_in, addr_Rsrc2_in, addr_Rdst_in,
-				     aluout,src1,immediate,
+				     aluout,src1_in,immediate_in,
 				     pc_in,
-				     writeback_in, ldm_in, port_read_in, mem_to_reg_in
-					);
+				     writeback_in, ldm_in, port_read_in, mem_to_reg_in,pc_to_stack_in,mem_write_in,mem_read_in,rti_in,int_in,ret_in,call_in,
+					 
+				     writeback_out, ldm_out, port_read_out, mem_to_reg_out,pc_to_stack_out,mem_write_out,mem_read_out,rti_out,int_out,ret_out,call_out,
+				     stack_out,
+				     src1add_out,scr2add_out,destadd_out,
+				     aluout_out,scr1_out,immediate_out,
+				     pc_out
+					 );
 		
 END ExStage; 
