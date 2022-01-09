32718d071779f3745ec3824df65cd394841faa2e
NouranHany <hany.noran@yahoo.com>
2022-01-09 02:49:13 +0200

    fixing the reset in the writeback buffer

--- Memory Stage/writeback_buffer.vhd
+++ Memory Stage/writeback_buffer.vhd
@@ -35,14 +35,10 @@ architecture arch1 of writeback_buffer is
 begin
 process(clk, rst)
 begin
-if (rst = '1' or empty_sp_exception_in = '1' or invalid_address_exception_in = '1') then
-	if (rst = '1') then
-		empty_sp_exception_out <= '0';
-		invalid_address_exception_out <= '0';
-	else
-		empty_sp_exception_out <= empty_sp_exception_in;
-		invalid_address_exception_out <= invalid_address_exception_in;
-	end if;
+if (rst = '1') then
+	
+	empty_sp_exception_out <= '0';
+	invalid_address_exception_out <= '0';
 	writeback_out <= '0';
 	ldm_out <= '0';
 	port_read_out <= '0';
@@ -57,23 +53,31 @@ if (rst = '1' or empty_sp_exception_in = '1' or invalid_address_exception_in = '
 	immediate_value_out <= (others => '0');
 	pc_out <= (others => '0');
 elsif (rising_edge(clk)) then
-	empty_sp_exception_out <= empty_sp_exception_in;
-	invalid_address_exception_out <= invalid_address_exception_in;
-	writeback_out <= writeback_in;
-	ldm_out <= ldm_in;
-	port_read_out <= port_read_in;
-	mem_to_reg_out <= mem_to_reg_in;
-	
-	addr_Rsrc1_out <= addr_Rsrc1_in;
-	addr_Rsrc2_out <= addr_Rsrc2_in;
-	addr_Rdst_out <= addr_Rdst_in;
+	if (empty_sp_exception_in = '1' or invalid_address_exception_in = '1') THEN
+		writeback_out <= '0';
+		ldm_out <= '0';
+		port_read_out <= '0';
+		mem_to_reg_out <= '0';
+		mem_to_pc_out <= '0';
+	else
+		empty_sp_exception_out <= empty_sp_exception_in;
+		invalid_address_exception_out <= invalid_address_exception_in;
+		writeback_out <= writeback_in;
+		ldm_out <= ldm_in;
+		port_read_out <= port_read_in;
+		mem_to_reg_out <= mem_to_reg_in;
+		
+		addr_Rsrc1_out <= addr_Rsrc1_in;
+		addr_Rsrc2_out <= addr_Rsrc2_in;
+		addr_Rdst_out <= addr_Rdst_in;
 
-	memory_data_out <= memory_data_in;
-	alu_result_out <= alu_result_in;
-	immediate_value_out <= immediate_value_in;
-	pc_out <= pc_in;
-	inport_val_out <= inport_val_in;
-	mem_to_pc_out <= mem_to_pc_in;
+		memory_data_out <= memory_data_in;
+		alu_result_out <= alu_result_in;
+		immediate_value_out <= immediate_value_in;
+		pc_out <= pc_in;
+		inport_val_out <= inport_val_in;
+		mem_to_pc_out <= mem_to_pc_in;
+	end if;
 end if;
 end process;
 end arch1;
